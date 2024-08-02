0ec7328af81f1928506c4dc08755b7bba17b7740
Ruby He <dreamahourse@gmail.com>
2024-08-02 13:41:21 +0800

    Handle errors

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const error = useRouteError();
+  return (
+    <>
+      <NavBar />
+      <Heading>Oops</Heading>
+      <Text>
+        {isRouteErrorResponse(error)
+          ? "The page is does not exist"
+          : "Unexpected error occurred"}
+      </Text>
+    </>
+  );
 };
 
 export default ErrorPage;
