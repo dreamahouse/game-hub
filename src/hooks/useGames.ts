626db8dfcda87d228ff0442977a3945d2718beaf
Ruby He <dreamahourse@gmail.com>
2024-08-05 16:33:54 +0800

    Modify the entities

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,7 +3,7 @@ import { GameQuery } from "../store";
 import APIClient from "../services/api-client";
 import { GetResponse } from "../services/api-client";
 import ms from "ms";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
 const useGames = (gameQuery: GameQuery) =>
